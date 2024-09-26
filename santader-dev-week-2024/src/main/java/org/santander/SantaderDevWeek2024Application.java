6623224a78db64aca249fd6649a9a7c6425a49b0
josue.dosidiana <josue.dosidiana@gmail.com>
2024-09-26 13:10:36 +0100

    primeiro commit

---
+++ santader-dev-week-2024/src/main/java/org/santander/SantaderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package org.santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderDevWeek2024Application.class, args);
+	}
+
+}
